f00d991281258ac46f9b8d64cad4ed508aff49ed
Shubham Garg <39940156+shubhgsgarg@users.noreply.github.com>
2020-09-06 15:34:33 +0530

    Update comparecompany.component.ts

--- src/app/comparecompany/comparecompany.component.ts
+++ src/app/comparecompany/comparecompany.component.ts
@@ -1,125 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { $, element } from 'protractor';
-import { Sector } from '../sector';
-import { CompanyserviceService } from '../companyservice.service';
-import { Company } from '../company';
-import { CompanyStockPrice } from '../company-stock-price';
-import {RendermapComponent} from '../rendermap/rendermap.component'
-import {SharedservicemapService} from '../sharedservicemap.service'
-
+import {LoginserviceService} from '../loginservice.service';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 @Component({
   selector: 'app-comparecompany',
   templateUrl: './comparecompany.component.html',
-  styleUrls: ['./comparecompany.component.css'],
-  providers: [RendermapComponent]
+  styleUrls: ['./comparecompany.component.css']
 })
 export class ComparecompanyComponent implements OnInit {
+  islogged:string;
 
-  companies: Company[];
-  stockPrices: CompanyStockPrice[];
-
-  public companyvalue: string;
-  public stockexchangevalue: string;
-  public fromperiod: string;
-  public toperiod: string;
-  public periodicity: number;
-
-  public companyList: number[] = [];
-  public obj : any;
-  public formdata: JSON;
-
-  public companyId: number;
-  public companyCode: number;
-  public showMap: boolean = false;
-
-  public lolCompanyStockPrice: CompanyStockPrice[][] = [];
-  fromPeriodList = [];
-  toPeriodList = [];
-  periodicityList=[];
-  companyNameList=[];
-
-  
-  constructor(private companyService : CompanyserviceService,
-    private sharedservicemap: SharedservicemapService,
-    ) { }
-
-  ngOnInit(): void {
-    this.companyService.findAllCompanies().subscribe(data => { 
-      this.companies = data
-      //console.log(data);
-    });
- 
-    
+  constructor(private loginserviceService: LoginserviceService,
+    private router: Router){
   }
-  
-  saveFormData(): void{
-    //console.log(this.companyvalue);
-    //console.log(this.stockexchangevalue);
-    //console.log(this.fromperiod);
-    //console.log(this.toperiod);
-    //console.log(this.periodicity);
-    
-    this.companies.forEach(element => {
-      //console.log(element.companyName);
-      //console.log(this.companyvalue);
-      if(element.companyName == this.companyvalue)
-      {
-        this.companyCode = element.companyCode;
-        //console.log(element.companyCode);
-      }
-    });
-    //console.log(this.companyCode);
-    this.companyList.push(this.companyCode);
-  
-    this.obj = {
-      "companyList": this.companyList,
-      "from": this.fromperiod,
-      "to": this.toperiod,
-      "periodicity": Number(this.periodicity)
-    };
 
-    this.formdata = <JSON>this.obj;
-    // console.log(this.formdata);
+  getLoginDetails(): string {
+    this.islogged= this.loginserviceService.currentUserValue();
+    return this.islogged;
+  }
 
-    this.companyService.findStockPrices(this.obj).subscribe(data => {
-      this.stockPrices=data;
-      // console.log(data);
-      this.lolCompanyStockPrice.push(this.stockPrices);
-    });
-    
-    
-    this.fromPeriodList.push(this.obj.from);
-    this.toPeriodList.push(this.obj.to);
-    if(this.obj.periodicity){
-      this.periodicityList.push(this.obj.periodicity)
+  ngOnInit() {
+    this.islogged=this.getLoginDetails();
+    if(this.islogged!="user" ){
+      this.loginserviceService.resetvalue();
+      this.router.navigate(['/login']);
+      return;
     }
-
-    this.companyList = [];
-  
-  
   }
-
-  sendData(): void{
-
-
-      this.sharedservicemap.stockprices1 = this.lolCompanyStockPrice[0];
-      this.sharedservicemap.stockprices2 = this.lolCompanyStockPrice[1]
-      this.sharedservicemap.company1 = ''
-      this.sharedservicemap.company2 = ''
-      this.sharedservicemap.isSamePeriod = true
-
-      if(this.periodicityList.length ==0){
-        if(this.fromPeriodList[0]!=this.fromPeriodList[1] || this.toPeriodList[0]!=this.toPeriodList[1]){
-          this.sharedservicemap.isSamePeriod = false;
-        }
-      }
-
-      
-      this.showMap=true;     
-      this.sharedservicemap.callMethodOfSecondComponent("random");
- 
-
-  
-  }
-
-  }
+}
