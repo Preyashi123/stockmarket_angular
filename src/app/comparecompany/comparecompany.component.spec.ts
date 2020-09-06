587a3a90e549588e8cbc9afb49c2b6bb94e20bb6
unknown <c2wfjava10@iiht.tech>
2020-09-06 15:03:07 +0530

    render map, compare company, sharedservicemap

--- src/app/comparecompany/comparecompany.component.spec.ts
+++ src/app/comparecompany/comparecompany.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComparecompanyComponent } from './comparecompany.component';
 
@@ -6,12 +6,12 @@ describe('ComparecompanyComponent', () => {
   let component: ComparecompanyComponent;
   let fixture: ComponentFixture<ComparecompanyComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ ComparecompanyComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ComparecompanyComponent);
--- src/app/rendermap/rendermap.component.ts
+++ src/app/rendermap/rendermap.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, } from '@angular/core';
 import {Chart} from 'chart.js'
 import { FormBuilder } from "@angular/forms";
-import {ExportService} from 'src/app/export.service'
+import {ExportService} from '../export.service'
+import {SharedservicemapService} from '../sharedservicemap.service'
 
 @Component({
   selector: 'app-rendermap',
@@ -11,180 +12,11 @@ import {ExportService} from 'src/app/export.service'
 export class RendermapComponent implements OnInit {
 
 
+
   title = 'Stock Exchange';
-  chart1=[];
-  chart2=[];
+  chart1;
+  chart2;
 
-
-  // responses1,responses2,label1,label2,isSame period contains dummy data which gets reset 
-  // once setResponse() functions is called from compareCompany or compareSector module
-  responses1 = [
-    {
-        "companyStockPriceId": 1,
-        "companyCode": 1002,
-        "stockExchange": "nsc",
-        "currentPrice": 10.7,
-        "date": "2020-01-28",
-        "time": "12:15:00"
-    },
-    {
-        "companyStockPriceId": 2,
-        "companyCode": 1002,
-        "stockExchange": "nsc",
-        "currentPrice": 50.7,
-        "date": "2020-02-28",
-        "time": "12:15:00"
-    },
-    {
-        "companyStockPriceId": 3,
-        "companyCode": 1002,
-        "stockExchange": "nsc",
-        "currentPrice": 30.7,
-        "date": "2020-03-03",
-        "time": "12:15:00"
-    },
-    {
-      "companyStockPriceId": 1,
-      "companyCode": 1002,
-      "stockExchange": "nsc",
-      "currentPrice": 10.7,
-      "date": "2020-04-28",
-      "time": "12:15:00"
-    },
-    {
-        "companyStockPriceId": 2,
-        "companyCode": 1002,
-        "stockExchange": "nsc",
-        "currentPrice": 50.7,
-        "date": "2020-05-28",
-        "time": "12:15:00"
-    },
-    {
-        "companyStockPriceId": 3,
-        "companyCode": 1002,
-        "stockExchange": "nsc",
-        "currentPrice": 30.7,
-        "date": "2020-06-03",
-        "time": "12:15:00"
-    },
-    {
-      "companyStockPriceId": 3,
-      "companyCode": 1002,
-      "stockExchange": "nsc",
-      "currentPrice": 10.7,
-      "date": "2020-06-04",
-      "time": "12:15:00"
-    },
-    {
-      "companyStockPriceId": 1,
-      "companyCode": 1002,
-      "stockExchange": "nsc",
-      "currentPrice": 20.7,
-      "date": "2020-07-28",
-      "time": "12:15:00"
-    },
-    {
-      "companyStockPriceId": 2,
-      "companyCode": 1002,
-      "stockExchange": "nsc",
-      "currentPrice": 50.7,
-      "date": "2020-08-28",
-      "time": "12:15:00"
-    },
-    {
-      "companyStockPriceId": 3,
-      "companyCode": 1002,
-      "stockExchange": "nsc",
-      "currentPrice": 30.7,
-      "date": "2020-09-03",
-      "time": "12:15:00"
-    },
-  ]
-  responses2 = [
-      {
-          "companyStockPriceId": 1,
-          "companyCode": 1003,
-          "stockExchange": "nsc",
-          "currentPrice": 70.7,
-          "date": "2020-01-28",
-          "time": "12:15:00"
-      },
-      {
-          "companyStockPriceId": 2,
-          "companyCode": 1003,
-          "stockExchange": "nsc",
-          "currentPrice": 20.7,
-          "date": "2020-02-28",
-          "time": "12:15:00"
-      },
-      {
-          "companyStockPriceId": 3,
-          "companyCode": 1003,
-          "stockExchange": "nsc",
-          "currentPrice": 45.7,
-          "date": "2020-03-03",
-          "time": "12:15:00"
-      },
-      {
-        "companyStockPriceId": 1,
-        "companyCode": 1003,
-        "stockExchange": "nsc",
-        "currentPrice": 70.7,
-        "date": "2020-04-28",
-        "time": "12:15:00"
-      },
-      {
-          "companyStockPriceId": 2,
-          "companyCode": 1003,
-          "stockExchange": "nsc",
-          "currentPrice": 20.7,
-          "date": "2020-05-28",
-          "time": "12:15:00"
-      },
-      {
-          "companyStockPriceId": 3,
-          "companyCode": 1003,
-          "stockExchange": "nsc",
-          "currentPrice": 45.7,
-          "date": "2020-06-01",
-          "time": "12:15:00"
-      },
-      {
-        "companyStockPriceId": 3,
-        "companyCode": 1003,
-        "stockExchange": "nsc",
-        "currentPrice": 45.7,
-        "date": "2020-06-04",
-        "time": "12:15:00"
-      },
-      {
-        "companyStockPriceId": 1,
-        "companyCode": 1003,
-        "stockExchange": "nsc",
-        "currentPrice": 10.7,
-        "date": "2020-07-28",
-        "time": "12:15:00"
-      },
-      {
-        "companyStockPriceId": 2,
-        "companyCode": 1003,
-        "stockExchange": "nsc",
-        "currentPrice": 10.7,
-        "date": "2020-08-28",
-        "time": "12:15:00"
-      },
-      {
-        "companyStockPriceId": 3,
-        "companyCode": 1003,
-        "stockExchange": "nsc",
-        "currentPrice": 10.7,
-        "date": "2020-09-03",
-        "time": "12:15:00"
-      },
-  ]
-  label1 ="Company ABC";
-  label2 ="Company XYZ";
-  isSamePeriod = true;
 
   xlabels1 = [];
   xlabels2 = [];
@@ -200,36 +32,47 @@ export class RendermapComponent implements OnInit {
   //these are default chart parameter which are present on load of page
   charttype= 'line';
   chartnumber= 1;
+  responses1: any;
+  responses2: any[];
+  label1: string;
+  label2: string;
+  isSamePeriod: boolean;
+  showSecondChart: boolean =true;
 
 
 
-
-//  <button (click)="getData()">Fetch Data</button>
-
-  setResponse( res1, res2, company1,company2, isSamePeriod)
+  setResponse( )
   {
-    this.responses1 = res1;
-    this.responses2 = res2;
-    this.label1 = company1;
-    this.label2 = company2;
-    this.isSamePeriod = isSamePeriod;
+    console.log("setresponse called");
+    this.responses1 = this.sharedservicemap.stockprices1;
+    this.responses2 = this.sharedservicemap.stockprices2;
+    this.label1 = this.sharedservicemap.company1;
+    this.label2 = this.sharedservicemap.company2;
+    this.isSamePeriod = this.sharedservicemap.isSamePeriod;
 
     if(this.isSamePeriod){
       this.fillMissingData();
-      this.createSingleChart();
     }
     else{
       this.fillVar();
-      this.createTwoChart();
+      
     }
     
- 
+    this.createTwoChart();
     this.calculateAvg();
   }
 
   graphtype: any = ['line', 'bar'];
 
-  constructor(public fb: FormBuilder, private exportService: ExportService) { }
+  constructor(  public fb: FormBuilder,
+    private exportService: ExportService,
+    private sharedservicemap: SharedservicemapService,
+    ) { 
+
+      this.sharedservicemap.invokeEvent.subscribe(value => {
+         this.setResponse();
+      }); 
+    }
 
   graphtypeForm = this.fb.group({
     name: "",
@@ -239,16 +82,19 @@ export class RendermapComponent implements OnInit {
   onSubmit() {
     this.charttype = this.graphtypeForm.value.name;
     this.chartnumber = this.graphtypeForm.value.number;
-    this.chart1=null;
-    this.chart2= null;
+    this.showSecondChart =true;
 
     if(this.chartnumber==1 && this.isSamePeriod==true){
-   
+      this.showSecondChart =false;
       this.createSingleChart();
     }
     else if(this.chartnumber==2){
+      // this.showSecondChart =true;
+      console.log(this.showSecondChart)
       this.createTwoChart();
+ 
     }
+   
     
   }
 
@@ -256,15 +102,15 @@ export class RendermapComponent implements OnInit {
   ngOnInit()
 
   {
-    if(this.isSamePeriod){
-      this.fillMissingData();
-      this.createSingleChart();
-    }
-    else{
-      this.fillVar();
-      this.createTwoChart();
-    }
-    this.calculateAvg();
+    // if(this.isSamePeriod){
+    //   this.fillMissingData();
+    //   this.createSingleChart();
+    // }
+    // else{
+    //   this.fillVar();
+    //   this.createTwoChart();
+    // }
+    // // this.calculateAvg();
     
   }
 
@@ -272,7 +118,10 @@ export class RendermapComponent implements OnInit {
   createSingleChart() {
     this.chart1=null;
     this.chart2= [];
-    this.chart1 = new Chart('chart1', {
+    var canvas1 = document.getElementById('chart1') as HTMLCanvasElement
+    var ctx1 = canvas1.getContext('2d');
+
+    this.chart1 = new Chart(ctx1, {
       type: this.charttype,
       data: {
         labels: this.xlabels1,
@@ -334,9 +183,14 @@ export class RendermapComponent implements OnInit {
   }
 
   createTwoChart() {
-    this.chart1=null;
-    this.chart2= null;
-    this.chart1 = new Chart('chart1', {
+
+    // this.chart1=null;
+    // this.chart2= null;
+    var canvas1 = document.getElementById('chart1') as HTMLCanvasElement
+    var ctx1 = canvas1.getContext('2d');
+    var canvas2 = document.getElementById('chart2') as HTMLCanvasElement
+    var ctx2 = canvas2.getContext('2d');
+    this.chart1 = new Chart(ctx1, {
       type: this.charttype,
       data: {
         labels: this.xlabels1,
@@ -373,7 +227,7 @@ export class RendermapComponent implements OnInit {
         }
       }
     });
-    this.chart2 = new Chart('chart2', {
+    this.chart2 = new Chart(ctx2, {
       type: this.charttype,
       data: {
         labels: this.xlabels2,
@@ -410,26 +264,27 @@ export class RendermapComponent implements OnInit {
         }
       }
     });
+    
   }
 
 
   private fillVar() 
   {
- 
-    for (var i=0;i<this.responses1.length || i<this.responses2.length;i++) {
-      if(i<this.responses1.length) {
-        this.data1.push(this.responses1[i].currentPrice);
-        this.xlabels1.push(this.responses1[i].date);
+    if(this.responses1.length || this.responses2.length){
+      for (var i=0;i<this.responses1.length || i<this.responses2.length;i++) {
+        if(i<this.responses1.length) {
+          this.data1.push(this.responses1[i].currentPrice);
+          this.xlabels1.push(this.responses1[i].date);
 
-      } 
-      if(i<this.responses2.length) {
-        this.data2.push(this.responses2[i].currentPrice);
-        this.xlabels2.push(this.responses2[i].date);
-      
+        } 
+        if(i<this.responses2.length) {
+          this.data2.push(this.responses2[i].currentPrice);
+          this.xlabels2.push(this.responses2[i].date);
+        
+        }
+        
       }
-      
-  }
-
+    }
 
     // for(let s of this.s1){
     //   this.xlabels1.push(s);
@@ -460,66 +315,67 @@ export class RendermapComponent implements OnInit {
 
 
   fillMissingData(){
-    var l1= this.responses1.length;
-    var l2 = this.responses2.length;
-    var maxl = l1<l2?l2:l1;
-    var tempData1 =[];
-    var tempData2 =[];
-    var tempXlabel =[];
-    var c1ptr=0;
-    var c2ptr=0;
-    var flag = true;
+    if(this.responses1.length || this.responses2.length){   
+      var l1= this.responses1.length;
+      var l2 = this.responses2.length;
+      var maxl = l1<l2?l2:l1;
+      var tempData1 =[];
+      var tempData2 =[];
+      var tempXlabel =[];
+      var c1ptr=0;
+      var c2ptr=0;
+      var flag = true;
 
-    while(flag){
+      while(flag){
 
-      if(c1ptr==l1 || c2ptr==l2) {
+        if(c1ptr==l1 || c2ptr==l2) {
 
-        flag =false; break;}
-      else if(this.responses1[c1ptr].date==this.responses2[c2ptr].date){
-      
-        tempData1.push(this.responses1[c1ptr].currentPrice)
-        tempData2.push(this.responses2[c2ptr].currentPrice)
-        tempXlabel.push(this.responses1[c1ptr].date)
-        c1ptr++
-        c2ptr++
+          flag =false; break;}
+        else if(this.responses1[c1ptr].date==this.responses2[c2ptr].date){
+        
+          tempData1.push(this.responses1[c1ptr].currentPrice)
+          tempData2.push(this.responses2[c2ptr].currentPrice)
+          tempXlabel.push(this.responses1[c1ptr].date)
+          c1ptr++
+          c2ptr++
+        }
+        else if(this.responses1[c1ptr].date<this.responses2[c2ptr].date){
+
+          tempData1.push(this.responses1[c1ptr].currentPrice)
+          tempData2.push(NaN)
+          tempXlabel.push(this.responses1[c1ptr].date)
+          c1ptr++
+        }
+        else if(this.responses1[c1ptr].date>this.responses2[c2ptr].date){
+
+          tempData1.push(NaN)
+          tempData2.push(this.responses2[c2ptr].currentPrice)
+          tempXlabel.push(this.responses2[c2ptr].date)
+          c2ptr++
+        }
       }
-      else if(this.responses1[c1ptr].date<this.responses2[c2ptr].date){
+      if(c1ptr<l1){
+        while(c1ptr<l1){
 
-        tempData1.push(this.responses1[c1ptr].currentPrice)
-        tempData2.push(NaN)
-        tempXlabel.push(this.responses1[c1ptr].date)
-        c1ptr++
+          tempData1.push(this.responses1[c1ptr].currentPrice)
+          tempData2.push(NaN)
+          tempXlabel.push(this.responses1[c1ptr].date)
+          c1ptr++
+        }
       }
-      else if(this.responses1[c1ptr].date>this.responses2[c2ptr].date){
+      else if(c2ptr<l2){
 
         tempData1.push(NaN)
         tempData2.push(this.responses2[c2ptr].currentPrice)
         tempXlabel.push(this.responses2[c2ptr].date)
         c2ptr++
       }
+      this.data1 = tempData1;
+      this.data2 = tempData2;
+      this.xlabels1 = tempXlabel;
+      this.xlabels2 = tempXlabel;
     }
-    if(c1ptr<l1){
-      while(c1ptr<l1){
-
-        tempData1.push(this.responses1[c1ptr].currentPrice)
-        tempData2.push(NaN)
-        tempXlabel.push(this.responses1[c1ptr].date)
-        c1ptr++
-      }
-    }
-    else if(c2ptr<l2){
-
-      tempData1.push(NaN)
-      tempData2.push(this.responses2[c2ptr].currentPrice)
-      tempXlabel.push(this.responses2[c2ptr].date)
-      c2ptr++
-    }
-    this.data1 = tempData1;
-    this.data2 = tempData2;
-    this.xlabels1 = tempXlabel;
-    this.xlabels2 = tempXlabel;
 
   }
-
 
 }
